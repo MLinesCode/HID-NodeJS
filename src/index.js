314e27c0b717844be1855e55f8639a5cb0ee2fae
zcMike <mikexx1264@gmail.com>
2019-11-08 23:58:03 -0600

    Hotfix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 // settings
 app.set('port', 3000)
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // middlewares
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'First Website' });
+  res.render('index.html', { title: 'First Website' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact.html', { title: 'Contact page'});
 });
 
 module.exports = router;
