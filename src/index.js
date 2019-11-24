1010e26beed862d565884f266af10e55849df889
Specter <mikexx1264@gmail.com>
2019-11-24 02:27:03 -0600

    Heroku deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // settings
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
