cc08fb3b7afdc5fba94b27b89f3bd3844a0daba8
Specter <mikexx1264@gmail.com>
2019-11-24 01:41:02 -0600

    HotfiX

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index.html', { title: 'First Website' });
+  res.render('index.html', { title: 'Falcon Heavy' });
 });
 
 router.get('/contact', (req, res) => {
